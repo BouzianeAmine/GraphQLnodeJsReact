1b8fd108aa682308d0371eee1e29bdeb4ca610d4
amine <aminebouz84@gmail.com>
2019-04-15 16:09:36 +0200

    confuguring done for the query and mutation schemas

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const express = require("express");
 const parser = require("body-parser");
+const graphqlhttp = require("express-graphql");
+const {
+    buildSchema
+} = require('graphql');
 const port = 5000;
 const app = express();
 
 app.use(parser.json());
 
 
+app.use('/graphql', graphqlhttp({
+    schema: buildSchema(`
+
+        type RootQuery {
+            cours:[String!]!
+        }
+
+        type RootMutation{
+            createCours(name:String): String
+
+        }
+
+        schema {
+            query: RootQuery
+            mutation:RootMutation
+        }   
+    `),
+    rootValue: {
+        cours: () => {
+            return ['Math', 'Java', 'Anglais'];
+        },
+
+        createCours: (args) => {
+            const name = args.name;
+            return name;
+        }
+
+    },
+
+    graphiql: true
+
+}));
 
 app.listen(port, () => {
     console.log("listennng on :",
