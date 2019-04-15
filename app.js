35fc68340a12b4cd48d768ae4c0962365f07946a
amine <aminebouz84@gmail.com>
2019-04-15 15:20:19 +0200

    first steps o the server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const parser = require("body-parser");
+const port = 5000;
+const app = express();
+
+app.use(parser.json());
+
+app.listen(port, () => {
+    console.log("listennng on :",
+        port)
+});
