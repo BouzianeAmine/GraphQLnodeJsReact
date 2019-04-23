2ae1cd75dc5469f470656d2cf049c3203ab16d3a
amine <aminebouz84@gmail.com>
2019-04-23 16:16:14 +0200

    add the front end side

--- ihm/src/App.js
+++ ihm/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Auth from "./pages/Auth";
 
-function App() {
-  return ( <
-    <
-    <
-    div > < /div>
-
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={Auth} />
+          <Route path="/cours" component={null} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ ihm/src/pages/Auth.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Auth extends Component {
+  render() {
+    return <h1> Auth route </h1>;
+  }
+}
+export default Auth;
