fbe58dab6617d7420fdcfde0fc13e45c38d5a778
amine <aminebouz84@gmail.com>
2019-04-25 17:13:21 +0200

    Done adding auth features to the front end , but no styling envoleved too ugly

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const graphqlhttp = require("express-graphql");
 const resolvers = require("./graphql/resolvers");
 const schema = require("./graphql/schema");
 const is_sign = require("./middlewear/is-signin");
-
+const cors = require("cors");
 const port = 5000;
 const app = express();
 
 const mongoose = require("mongoose");
 
+app.use(cors());
 app.use(is_sign);
 app.use(parser.json());
 app.use(express.json());
--- ihm/src/pages/Auth.js
+++ ihm/src/pages/Auth.js
@@ -1,8 +1,63 @@
 import React, { Component } from "react";
 
 class Auth extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passEl = React.createRef();
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+    const body = {
+      query: `
+        query {
+          login(email:"${email}",password:"${password}"){
+            userId
+          }
+        }
+      `
+    };
+    fetch("http://localhost:5000/graphql", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(resbody => {
+        console.log(resbody);
+      });
+  };
+
   render() {
-    return <h1> Auth route </h1>;
+    return (
+      <form onSubmit={this.submitHandler}>
+        <div className="content-el">
+          <label htmlFor="email">Email</label>
+          <input type="email" ref={this.emailEl} />
+          <label htmlFor="password">Password</label>
+          <input type="password" ref={this.passEl} />
+        </div>
+        <div className="action-el">
+          <button type="button" value="signup">
+            signup
+          </button>
+          <button type="submit" value="signin">
+            signin
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 export default Auth;
